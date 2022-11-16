fb669e22061b68a652c428aa9c64747e85e7bf31
hidihyeonee <59243729+leejihyeon240@users.noreply.github.com>
2022-11-16 20:45:59 +0900

    버튼 개선하기

--- Mini-Game-App/screens/StartGameScreen.js
+++ Mini-Game-App/screens/StartGameScreen.js
@@ -10,8 +10,14 @@ function StartGameScreen() {
             keyboardType="number-pad"
             autoCapitalize="none"
             autoCorrect={false} />
-            <PrimaryButton>Reset</PrimaryButton>
-            <PrimaryButton>Confirm</PrimaryButton>
+            <View style={styles.buttonsContainer}>
+                <View style={styles.buttonContainer}>
+                    <PrimaryButton>Reset</PrimaryButton>
+                </View>
+                <View style={styles.buttonContainer}>
+                    <PrimaryButton>Confirm</PrimaryButton>
+                </View>
+            </View>
         </View> 
         // maxLength는 최대 길이 수 제한인데, 숫자니까 "" 이거 말고, {} 이거 써야함
         // keyboardTypes는 숫자 패드만 뜨도록 하는 것
@@ -23,12 +29,15 @@ export default StartGameScreen;
 
 const styles = StyleSheet.create({
     inputContainer: {
+        justifyContent: 'center',
+        alignItems: 'center',
         marginTop: 100,
         marginHorizontal:24,
         padding: 16,
         backgroundColor: '#4e0329',
         borderRadius: 8,
-        // elevation = 안드로이드용 그림자
+        elevation: 4,
+        // 안드로이드용 그림자
         shadowColor: 'black',
         shadowOffset: { width: 0, height: 2},
         shadowRadius: 6,
@@ -44,6 +53,11 @@ const styles = StyleSheet.create({
         marginVertical: 8,
         fontWeight: 'bold',
         textAlign: 'center'
+    },
+    buttonsContainer: {
+        flexDirection: 'row'
+    },
+    buttonContainer: {
+        flex: 1
     }
-
 });
