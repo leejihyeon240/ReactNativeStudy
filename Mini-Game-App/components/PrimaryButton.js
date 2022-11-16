0860d1d541a65f17e5f02f9c50f6bd5ede45a4aa
hidihyeonee <59243729+leejihyeon240@users.noreply.github.com>
2022-11-16 20:27:51 +0900

    버튼에 시각적 피드백 추가하기

--- Mini-Game-App/components/PrimaryButton.js
+++ Mini-Game-App/components/PrimaryButton.js
@@ -1,11 +1,46 @@
-import { View,Text } from "react-native";
+import { View, Text, Pressable, StyleSheet } from "react-native";
 
 function PrimaryButton({ children }) {
+    function pressHandler() {
+        console.log('Pressed!');
+    }
+
     return (
-        <View>
-            <Text> {children} </Text>
-        </View>
+            <View style={styles.buttonOuterContainer}>
+                <Pressable 
+                style={({pressed}) => 
+                    pressed 
+                        ? [styles.buttonInnerContainer, styles.pressed] // 참일 때
+                        : styles.buttonInnerContainer
+                    } // 거짓이라면 컨테이너 스타일의 기본 버튼을 적용
+                    onPress={pressHandler}
+                    android_ripple={{color: '#640233'}}
+                > 
+                <Text style={styles.buttonText}> {children} </Text>
+                </Pressable>
+            </View>
     );
 }
 
 export default PrimaryButton;
+
+const styles = StyleSheet.create({
+    buttonOuterContainer: {
+        borderRadius: 28,
+        margin: 4,
+        overflow: 'hidden'
+    },
+    buttonInnerContainer: {
+        backgroundColor: '#ddb52f',
+        paddingVertical: 8,
+        paddingHorizontal: 16,
+        elevation: 2,
+    },
+    buttonText: {
+        color: 'white',
+        textAlign: 'center'
+    },
+    pressed: {
+        opacity: 0.75
+    }, // 물결 효과
+})
