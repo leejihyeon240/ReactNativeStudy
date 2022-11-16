f41f7ae356fdfc7e5437fca645e4221e0c089e6a
hidihyeonee <59243729+leejihyeon240@users.noreply.github.com>
2022-11-16 19:45:36 +0900

    TextInput 필드 구성

--- Mini-Game-App/screens/StartGameScreen.js
+++ Mini-Game-App/screens/StartGameScreen.js
@@ -3,11 +3,19 @@ import PrimaryButton from "../components/PrimaryButton";
 
 function StartGameScreen() {
     return (
-        <View style={styles.inputContainer}>
-            <TextInput />
+        <View style={styles.inputContainer}> 
+            <TextInput 
+            style={styles.numberInput} 
+            maxLength={2} 
+            keyboardType="number-pad"
+            autoCapitalize="none"
+            autoCorrect={false} />
             <PrimaryButton>Reset</PrimaryButton>
             <PrimaryButton>Confirm</PrimaryButton>
-        </View>
+        </View> 
+        // maxLength는 최대 길이 수 제한인데, 숫자니까 "" 이거 말고, {} 이거 써야함
+        // keyboardTypes는 숫자 패드만 뜨도록 하는 것
+        // autoCorrect 자동 대소문자나 자동 수정 끄기
     );
 }
 
@@ -25,6 +33,17 @@ const styles = StyleSheet.create({
         shadowOffset: { width: 0, height: 2},
         shadowRadius: 6,
         shadowOpacity: 0.25
+    },
+    numberInput: {
+        height: 50,
+        width: 50,
+        fontSize: 32,
+        borderBottomColor: '#ddb52f',
+        borderBottomWidth: 2,
+        color: '#ddb52f',
+        marginVertical: 8,
+        fontWeight: 'bold',
+        textAlign: 'center'
     }
 
 });
