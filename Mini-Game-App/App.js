dbee03d98fa3b9c6bdde6925b667b3414f99177b
hidihyeonee <59243729+leejihyeon240@users.noreply.github.com>
2022-11-16 20:51:12 +0900

    앱 전반에 색상 넣기(배경)

--- Mini-Game-App/App.js
+++ Mini-Game-App/App.js
@@ -1,9 +1,18 @@
-import { StyleSheet } from 'react-native';
+import { StyleSheet, View } from 'react-native';
 
 import StartGameScreen from './screens/StartGameScreen';
 
 export default function App() {
-  return <StartGameScreen />;
+  return (
+    <View style={styles.rootScreen}>
+      <StartGameScreen />
+    </View>
+  );
 }
 
-const styles = StyleSheet.create({});
+const styles = StyleSheet.create({
+    rootScreen: {
+      flex: 1, // flex 1을 추가하면 View가 스타일을 전달받아 최대한의 공간을 차지함
+      backgroundColor: '#ddb52f'
+    }
+});
