de618262e064abddefd92f0f51a936624b768e0a
hidihyeonee <59243729+leejihyeon240@users.noreply.github.com>
2022-12-04 00:11:37 +0900

    동적 너비 설정하기

--- Mini-Game-App/components/ui/Title.js
+++ Mini-Game-App/components/ui/Title.js
@@ -9,12 +9,14 @@ export default Title;
 const styles = StyleSheet.create({
     title: {
         fontFamily: 'open-sans-bold',
-        fontSize: 18,
+        fontSize: 24,
         // fontWeight: 'bold',
         color: 'white',
         textAlign: 'center',
         borderWidth: 2,
         borderColor: 'white',
-        padding: 12
+        padding: 12,
+        maxWidth: '80%', //  동적 너비 설정하기
+        width: 300
     }
 });
