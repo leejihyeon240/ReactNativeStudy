ca34b9850bdc4e1c82e1328563e24ed55a1ee046
hidihyeonee <59243729+leejihyeon240@users.noreply.github.com>
2022-12-11 00:18:00 +0900

    오류

--- other-navigators-starting-project/App.js
+++ other-navigators-starting-project/App.js
@@ -1,10 +1,18 @@
-import { createDrawerNavigator } from '@react-navigation/drawer';
 import { NavigationContainer } from '@react-navigation/native';
+import { createDrawerNavigator } from '@react-navigation/drawer';
+
+import WelcomScreen from './screens/WelcomeScreen';
+import UserScreen from './screens/UserScreen';
 
 const Drawer = createDrawerNavigator();
 
 export default function App() {
-  return <NavigationContainer>
-
-  </NavigationContainer>;
+  return (
+  <NavigationContainer>
+    <Drawer.Navigator initialRouteName='User'>
+      <Drawer.Screen name='Welcome' component={WelcomScreen} />
+      <Drawer.Screen name='User' component={UserScreen} />
+    </Drawer.Navigator>
+  </NavigationContainer>
+  );
 }
