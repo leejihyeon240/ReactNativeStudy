67b8862bb38a2a408ece87c1ad0b427af0a12cb2
hidihyeonee <59243729+leejihyeon240@users.noreply.github.com>
2022-12-10 23:57:05 +0900

    드로어 네비게이션 추가하기

--- other-navigators-starting-project/App.js
+++ other-navigators-starting-project/App.js
@@ -1,5 +1,10 @@
-import { Text } from 'react-native';
+import { createDrawerNavigator } from '@react-navigation/drawer';
+import { NavigationContainer } from '@react-navigation/native';
+
+const Drawer = createDrawerNavigator();
 
 export default function App() {
-  return <Text>Todo...</Text>;
+  return <NavigationContainer>
+
+  </NavigationContainer>;
 }
