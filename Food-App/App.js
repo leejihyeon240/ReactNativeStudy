c493f6d57754a8e56fbb954230270859bb5f8ecb
hidihyeonee <59243729+leejihyeon240@users.noreply.github.com>
2022-12-12 21:00:31 +0900

    other-navigators-starting-project

--- Food-App/App.js
+++ Food-App/App.js
@@ -1,7 +1,5 @@
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Button } from 'react-native';
-import { NavigationContainer } from '@react-navigation/native';
-import { createNativeStackNavigator } from '@react-navigation/native-stack';
 
 import CategorysScreen from './screens/CategoriesScreen';
 import MealsOverviewScreen from './screens/MealsOverviewScreen';
