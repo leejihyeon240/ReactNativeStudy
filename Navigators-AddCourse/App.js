6bb22cdc1b2176c26756a82345561b0065589431
hidihyeonee <59243729+leejihyeon240@users.noreply.github.com>
2022-12-14 04:18:39 +0900

    하단 탭 추가, 구성 & 사용하기

--- Navigators-AddCourse/App.js
+++ Navigators-AddCourse/App.js
@@ -1,16 +1,47 @@
 import { NavigationContainer } from '@react-navigation/native';
-import { createDrawerNavigator, DrawerContent } from '@react-navigation/drawer';
+import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
 import { Ionicons } from '@expo/vector-icons';
 
 import WelcomeScreen from './screens/WelcomeScreen';
 import UserScreen from './screens/UserScreen';
 
-const Drawer = createDrawerNavigator();
+const BottomTab = createBottomTabNavigator();
 
 export default function App() {
   return (
     <NavigationContainer>
-      <Drawer.Navigator initialRouteName='User'
+      <BottomTab.Navigator initialRouteName='User'
+        screenOptions={{
+          headerStyle: { backgroundColor: '#3c0a6b' },
+          headerTintColor: 'white',
+          tabBarActiveTintColor: '#3c0a6b'
+        }}>
+        <BottomTab.Screen
+          name='Welcome'
+          component={WelcomeScreen}
+          options={{
+            tabBarIcon: ({ color, size }) => (
+              <Ionicons name='home' color={color} size={size} />
+            )
+          }} />
+        <BottomTab.Screen
+          name='User'
+          component={UserScreen}
+          options={{
+            tabBarIcon: ({ color, size }) => (
+              <Ionicons name='person' color={color} size={size} />
+            )
+          }} />
+      </BottomTab.Navigator>
+    </NavigationContainer>
+  );
+}
+
+// import { createDrawerNavigator } from '@react-navigation/drawer';
+
+// const Drawer = createDrawerNavigator();
+
+/* <Drawer.Navigator initialRouteName='User'
         screenOptions={{
           headerStyle: { backgroundColor: '#3c0a6b' },
           headerTintColor: 'white',
@@ -32,7 +63,4 @@ export default function App() {
             drawerLabel: 'User',
             drawerIcon: ({ color, size }) => (<Ionicons name='person' color={color} size={size} />),
           }} />
-      </Drawer.Navigator>
-    </NavigationContainer>
-  );
-}
+      </Drawer.Navigator> */
