acc099054010a2679e35a6543ce1c569a5d5c705
hidihyeonee <59243729+leejihyeon240@users.noreply.github.com>
2022-12-16 00:17:00 +0900

    ConstextAPI를 통해 즐겨 찾는 음식 관리하기

--- Food-App_ReduxContextAPI/screens/FavoritesScreen.js
+++ Food-App_ReduxContextAPI/screens/FavoritesScreen.js
@@ -1,7 +1,37 @@
-import { Text } from 'react-native';
+import { useContext } from 'react';
+import { View, Text, StyleSheet } from 'react-native';
+
+import MealIsList from '../components/MealsList/MealsList';
+import { MEALS } from '../data/dummy-data';
+import { FavoritesContext } from '../store/context/favorites-context';
 
 function FavoritesScreen() {
-  return <Text>The favorites screen!</Text>;
+  const favoriteMealsCtx = useContext(FavoritesContext);
+
+  const favoriteMeals = MEALS.filter(meal => favoriteMealsCtx.ids.includes(meal.id)); // 즐겨찾기를 누른 아이템이 있다면 true를 반환
+
+  if (favoriteMeals.length === 0 ) { // 즐겨찾기에 아무 음식이 없을 때
+    return (
+      <View style={styles.rootContainer}>
+        <Text style={styles.text}> You have no favorite meals yet.</Text>
+      </View>
+    )
+  }
+  return <MealIsList items={favoriteMeals} />
 }
 
 export default FavoritesScreen;
+
+const styles = StyleSheet.create({
+  rootContainer: {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center'
+  },
+  text: {
+    fontSize: 18,
+    fontWeight: 'bold',
+    color: 'white'
+  }
+});
+
