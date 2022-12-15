1482ae879d8993417429d21ebf2e9db1aea9efd1
hidihyeonee <59243729+leejihyeon240@users.noreply.github.com>
2022-12-16 01:42:54 +0900

    컴포넌트 내에서 Redux 상태 사용하기

--- Food-App_ReduxContextAPI/screens/FavoritesScreen.js
+++ Food-App_ReduxContextAPI/screens/FavoritesScreen.js
@@ -1,14 +1,20 @@
-import { useContext } from 'react';
+// import { useContext } from 'react';
 import { View, Text, StyleSheet } from 'react-native';
+import { useSelector } from 'react-redux';
 
 import MealIsList from '../components/MealsList/MealsList';
 import { MEALS } from '../data/dummy-data';
-import { FavoritesContext } from '../store/context/favorites-context';
+// import { FavoritesContext } from '../store/context/favorites-context';
 
 function FavoritesScreen() {
-  const favoriteMealsCtx = useContext(FavoritesContext);
+  // const favoriteMealsCtx = useContext(FavoritesContext);
 
-  const favoriteMeals = MEALS.filter(meal => favoriteMealsCtx.ids.includes(meal.id)); // 즐겨찾기를 누른 아이템이 있다면 true를 반환
+  const favoriteMealIds = useSelector((state) => state.favoriteMeals.ids);
+
+  // const favoriteMeals = MEALS.filter(meal => favoriteMealsCtx.ids.includes(meal.id)); // 즐겨찾기를 누른 아이템이 있다면 true를 반환
+
+  
+  const favoriteMeals = MEALS.filter((meal) => favoriteMealIds.includes(meal.id)); // 즐겨찾기를 누른 아이템이 있다면 true를 반환
 
   if (favoriteMeals.length === 0 ) { // 즐겨찾기에 아무 음식이 없을 때
     return (
